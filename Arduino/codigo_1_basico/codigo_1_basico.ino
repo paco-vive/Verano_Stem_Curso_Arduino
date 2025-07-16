38a389c898501d2a1cddd97904ebe07d242391d0
paco-vive <francisco.viverosma@udlap.mx>
2025-07-16 11:08:15 -0600

    Add files via upload

---
+++ Arduino/codigo_1_basico/codigo_1_basico.ino
@@ -0,0 +1,56 @@
+const int in1 = 6;
+const int in2 = 7;
+const int in3 = 12;
+const int in4 = 13;
+
+char comando;
+
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  adelante();
+  delay(500);
+  atras();
+  delay(500);
+}
+
+void adelante() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void atras() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void izquierda() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void derecha() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void detener() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
